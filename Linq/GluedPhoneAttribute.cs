55a1834c1b6549dba3f7b616751c281fb4a2b29a
Stepan Myroniuk <myronyuk.s@gmail.com>
2017-07-13 23:55:39 +0300

    First commit

---
+++ Linq/GluedPhoneAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Linq {
+	[AttributeUsage(AttributeTargets.Property, AllowMultiple = true, Inherited = false)]
+	public class GluedPhoneAttribute : ValidationAttribute {
+		private const string Pattern = @"^(1\s*[-\/\.]?)?(\((\d{3})\)|(\d{3}))\s*[-\/\.]?\s*(\d{3})\s*[-\/\.]?\s*(\d{4})\s*(([xX]|[eE][xX][tT])\.?\s*(\d+))*$";
+
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext) {
+			bool isValid = false;
+
+			string stringValue = null;
+			if (value != null)
+				stringValue = value.ToString();
+
+			if (string.IsNullOrEmpty(stringValue) || Regex.IsMatch(stringValue, Pattern))
+				isValid = true;
+
+			return isValid
+				? ValidationResult.Success
+				: new ValidationResult("Incorrect number");
+		}
+	}
+}
---
+++ Linq/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Linq {
+	class Program {
+		static void Main(string[] args) {
+			var user = new User
+			{
+				FirstName = "Stepan",
+				LastName = "Myr",
+				IsActive = false
+			};
+
+			var phone = new Phone
+			{
+				CountryId = 32,
+				Number = "+380977898999",
+				IsActive = true
+			};
+
+			HtmlHelper<User> userHelper = new HtmlHelper<User>(new ViewContext(), new ViewPage{ViewData = new ViewDataDictionary{Model = user}});
+			HtmlHelper<Phone> phoneHelper = new HtmlHelper<Phone>(new ViewContext(), new ViewPage{ ViewData = new ViewDataDictionary{Model = phone}});
+
+			//Invoke with custom functions
+			var activeValueForUser = GetValueWithFunc(userHelper, ReturnUserIsActive);
+			var activeValueForPhone = GetValueWithFunc(phoneHelper, ReturnPhoneIsActive);
+
+			//Invoke with lamba functions
+			activeValueForUser = GetValueWithFunc(userHelper, t => t.IsActive);
+			activeValueForPhone = GetValueWithFunc(phoneHelper, t => t.IsActive);
+
+			//Invoke with expressions
+			var phoneValueForPhone = GetValueWithExpression(phoneHelper, t => t.Number);
+		}
+
+		//Take Func. We can't get metadata for properties
+		public static string GetValueWithFunc<TModel, TProperty>(HtmlHelper<TModel> htmlHelper, Func<TModel, TProperty> expression) {
+
+			object value = expression(htmlHelper.ViewData.Model);
+
+			string strLabel = "-";
+			if (value != null) {
+				if (value is bool) {
+					if ((bool)value) {
+						strLabel = "Yes";
+					} else {
+						strLabel = "No";
+					}
+				} else {
+					strLabel = value.ToString();
+				}
+			}
+
+			return strLabel;
+		}
+
+		public static string GetValueWithExpression<TModel, TProperty>(HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression) {
+			ModelMetadata metadata = ModelMetadata.FromLambdaExpression(expression, htmlHelper.ViewData);
+			var value = metadata.Model;
+
+			string strLabel = "-";
+			if (value != null) {
+				if (value is bool) {
+					if ((bool)value) {
+						strLabel = "Yes";
+					} else {
+						strLabel = "No";
+					}
+				} else {
+					strLabel = value.ToString();
+				}
+
+				if (metadata.AdditionalValues.ContainsKey("PhoneNumber"))
+				{
+					strLabel = strLabel + "<a href=\"callto://" + strLabel.Replace(" ", string.Empty) + "\" class=\"clear-underline outgoing-call\"><i class=\"icon-outgoing-call metro-color\"></i></a>";
+				}
+			}
+
+			return strLabel;
+		}
+
+		public static bool ReturnUserIsActive(User user)
+		{
+			return user.IsActive;
+		}
+
+		public static bool ReturnPhoneIsActive(Phone phone) {
+			return phone.IsActive;
+		}
+	}
+
+
+	class User {
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public bool IsActive { get; set; }
+	}
+
+	class Phone {
+		[GluedPhoneAttribute]
+		public string Number { get; set; }
+		public bool IsActive { get; set; }
+		public int CountryId { get; set; }
+	}
+}
